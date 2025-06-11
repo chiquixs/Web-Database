c1878f0fb883361eb1b6f140da61c08b32ef3e76
triaanandafh <fadiltria51@gmail.com>
2025-06-11 14:53:31 +0700

    tampilan homepage

--- index.php
+++ index.php
@@ -11,18 +11,17 @@
 </head>
 <body>
     <header>
-        <div class="logo">
-            <img src="image/logo.png" alt="logo rebant">
-        </div>
         <nav>
+            <div class="logo">
+                <img src="image/logo.png" alt="logo rebant">
+            </div>
             <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">about us</a></li>
-                <li><a href="produk.php">product</a></li>
-                <li><a href="membership.php">membership</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="produk.php">Product</a></li>
+                <li><a href="membership.php">Membership</a></li>
             </ul>
         </nav>
-        <a href="login.php" style="color: white; text-decoration: none;">👤</a>
     </header>
     <main>
     <section class="content">
@@ -37,6 +36,28 @@
         <div class="image">
             <img src="image/gambar1.jpg" alt="Dekorasi Dinding" />
         </div>
+    </section>
+
+    <section class="about-us">
+        <h1>ABOUT US</h1>
+        <div class="underline"></div>
+        <div class="desc">
+            <div class="about-img">
+                <img src="image/1.jpeg" alt="">
+            </div>
+            <div class="about-text">
+                <p><span>Curated Calm for Every Wall</span></p>
+                <p>At REBANT, we believe that your home should reflect your personality.
+                    We specialize in premium wall decor crafted from high-quality materials that are also environmentally friendly. Every piece we create is designed to elevate your interior with timeless elegance and sustainable values.
+                    From minimalist designs to nature-inspired artwork, our products are made to bring life, depth, and harmony into your space.
+                    Join thousands of satisfied customers who trust REBANT to transform their walls into statements of style and comfort.
+                </p>
+                <div class="button">
+            <button>Contact Us</button>
+        </div>
+            </div>
+        </div>
+        
     </section>
     </main>
 </body>
